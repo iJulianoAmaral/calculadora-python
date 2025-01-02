953400b52c01640826378847b1cf52222e21b8d2
iJulianoAmaral <149726384+iJulianoAmaral@users.noreply.github.com>
2025-01-02 15:39:08 -0300

    Script criado no módulo anterior

---
+++ nome.py
@@ -0,0 +1,2 @@
+nome = input('Digite Seu Nome: ')
+print('Seja bem vindo, ',nome)
---
+++ python_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install python3
+
+python3 /home/juliano/modulo1/python/nome.py
+
